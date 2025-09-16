5374118cca8c238285a65c8a7ee645dd48c0280c
Dang To <206424389+yomsi-thd@users.noreply.github.com>
2025-09-16 08:57:28 +0700

    Add operate function based on operation signs

--- src/script.js
+++ src/script.js
@@ -15,4 +15,18 @@ function divide(num1, num2){
 };
 
 
+let num1 = 0, num2 = 0, operator = "";
+
+function operate(num1, num2, operator){
+    switch (operator){
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
 
