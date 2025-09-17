ac048740355ef67c1e1102183b0efa7742e8c026
Dang To <206424389+yomsi-thd@users.noreply.github.com>
2025-09-17 08:27:49 +0700

    Add populateDisplay() related comments

--- src/script.js
+++ src/script.js
@@ -30,7 +30,11 @@ function operate(num1, num2, operator){
     }
 }
 
-//--Function that populate the display when you click the buttons. It should be stored the content --
+//--Function that populate the display when you click the buttons. It should stored the content of the display in a variable for use in the next step --
 //Create event listeners for every "number" and "operator" button.
-//Each button contains a string value of that "number" or operator
-//By default, the 
+//Each button contains a string value of that "number" or operator.
+//By default, the display should be setted at 0.
+//Upon a button press, if the current display value is "0", the number should be switched from "0" to the 'just-pressed' button's value. 
+//If the current display value is NOT "0", then the current display value should concatinate the 'just-pressed' button's value.
+//The display should only show 1 operator at a time and will not do 2 operations at the same time. 
+//Upon hitting the 'AC' clear button, the display should be resetted back to 0 
