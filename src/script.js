9affc3e14c2d033dc5c8290dfeb85e235c6b1150
Dang To <206424389+yomsi-thd@users.noreply.github.com>
2025-09-17 09:25:01 +0700

    Add click listener to update display

--- src/script.js
+++ src/script.js
@@ -30,11 +30,39 @@ function operate(num1, num2, operator){
     }
 }
 
+
+
+
 //--Function that populate the display when you click the buttons. It should stored the content of the display in a variable for use in the next step --
-//Create event listeners for every "number" and "operator" button.
-//Each button contains a string value of that "number" or operator.
 //By default, the display should be setted at 0.
-//Upon a button press, if the current display value is "0", the number should be switched from "0" to the 'just-pressed' button's value. 
-//If the current display value is NOT "0", then the current display value should concatinate the 'just-pressed' button's value.
+const DEFAULT_DISPLAY_VALUE = 0;
+const display = document.querySelector(".display");
+//Every "number" and "operator" button will have an event listener that will handle the display
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+numberButtons.forEach((numberButton) => numberButton.addEventListener("click", () =>  populateDisplay(numberButton.value))); //Each button contains a string value of that "number" or "operator".
+operatorButtons.forEach((operatorButton) => operatorButton.addEventListener("click", () =>  populateDisplay(operatorButton.value)));
+
+function populateDisplay(buttonValue){
+    //Upon a button press, If the current display value is NOT "0", then the current display value should concatinate the 'just-pressed' button's value. 
+    if (display.value != DEFAULT_DISPLAY_VALUE){
+        display.value += buttonValue;
+    }
+    //If the current display value is "0", the display value should be switched from "0" to the 'just-pressed' button's value. 
+    //If the display was 0 and an operator was pressed instead of the number, it will not replace the number "0" and instead concatenate the operator to the number "0"
+    if (display.value == DEFAULT_DISPLAY_VALUE){
+        if (buttonValue.includes("+" || "-" || "×" || "÷")) {
+            display.value += buttonValue;
+        }else{
+            display.value = buttonValue;
+        }
+    }
+}
+
+//Upon hitting the 'AC' clear button, the display should be resetted back to 0
+
+
+
 //The display should only show 1 operator at a time and will not do 2 operations at the same time. 
-//Upon hitting the 'AC' clear button, the display should be resetted back to 0 
+
+
