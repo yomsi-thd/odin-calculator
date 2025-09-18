09552d653af2ca695c88520caa2959fab9aa8b6b
Dang To <206424389+yomsi-thd@users.noreply.github.com>
2025-09-18 13:21:33 +0700

    Remove populateDisplay() comments for new approach
    The old comments & code were removed alongside the function
    populateDisplay(). The populateDisplay() should only change the display.
    Previous iteration attempted to calculate and change the display at the
    same time.
    
    The new solution now separe many functions behind a calculator
    seperately instead of stuffing it all in populateDisplay();
    
    The new soltion will include the following type of functions
    - Encoder (a function that will determine button types & values
    pressed)
    - Controller (handles the main logic to guarantee the correct display
    based on buttons pressed)
    - ALU (the operate() function that handles the math for the calculator)
    - Display Encoder (populateDisplay() function that shows the correct
    numbers on the screen)

--- src/script.js
+++ src/script.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 function add(num1 , num2){
     return num1 + num2;
 };
@@ -31,31 +37,7 @@ function operate(num1, num2, operator){
 }
 
 
-//--Function that populate the display when you click the buttons. It should stored the content of the display in a variable for use in the next step --
-//By default, the display should be setted at 0.
-const DEFAULT_DISPLAY_VALUE = 0;
-const display = document.querySelector(".display");
-//Every "number" and "operator" button will have an event listener that will handle the display
-const numberButtons = document.querySelectorAll(".number");
-const operatorButtons = document.querySelectorAll(".operator");
-numberButtons.forEach((numberButton) => numberButton.addEventListener("click", () =>  populateDisplay(numberButton.value))); //Each button contains a string value of that "number" or "operator".
-operatorButtons.forEach((operatorButton) => operatorButton.addEventListener("click", () =>  operator = operatorButton.value));
 
-function populateDisplay(buttonValue){
-    //Upon a button press, If the current display value is "0", the display value should be switched from "0" to the 'just-pressed' button's value. 
-    //This first value will be also stored in "num1"
-    //Upon clicking an operator, the first value is kept inside a variable, the operator button gets highlighted.
-    //The display will NOT display an operator on screen, however, the selected operator is kept tracked for calculation later. 
-    //Upon clicking on another number after clicking on an operator once, the display value should be switched "num1" to the 'just-presssed' button's value
-    //This second value will be stored in "num2"
-    //If there's 2 operators are clicked twice with at least 2 numbers entered, display the operated value AND highlight the button of the 2nd operator
-    //If 1 operator was click AND the "=" was pressed, display the operated value
-    
-}
-
-//Upon clicking the 'AC' clear button, the display should be resetted back to 0
-const clearButton = document.querySelector("#clear");
-clearButton.addEventListener("click", () => display.value = DEFAULT_DISPLAY_VALUE);
 
 
 
